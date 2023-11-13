eb4d94f4bb605a929827c3461fe2cd24134e09ee
NataliaArdilez <natyardilez>
2023-11-12 22:22:32 -0300

    Solucion de errores en app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
-app.get('/', (req, res) => {
+app.get('/detail', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'productDetail.html'))
 })
 
