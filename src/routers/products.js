5ced682eea046c740aefaf9689a425c4a8ea73c6
NataliaArdilez <natyardilez@gmail.com>
2023-12-12 05:34:42 -0300

    mejoras en css y solucion de errores en el form create

--- src/routers/products.js
+++ src/routers/products.js
@@ -14,7 +14,7 @@ let productController=require('../controllers/productController.js');
 router.get('/products', productController.getIndex);
 
 /*** CREATE ONE PRODUCT http://localhost:3000/products/create ***/
-router.get('/create', productController.getCreate);
+//router.get('/create', productController.getCreate);
 //router.post('/create', upload.single('image'),validateProducts, productController.store);
 
 
@@ -24,8 +24,12 @@ router.get('/cart', productController.getCart);
 
 
 
+/***Create one product http://localhost:5000/products/create ***/
 
-router.get('/create', productController.postCreate);
+router.get("/create", productController.create);
+router.post("/create", productController.store);
+//router.post("/create", upload.single('image'), productController.store);
+
 
 //router.post("/create", (req, res)=>(productController.postCreate));
 
